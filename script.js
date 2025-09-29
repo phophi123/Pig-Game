cca7ed155c4d2544746d6459ac433d80c457bb86
Phophi Muravha <Phophim@redpandasoftware.co.za>
2025-09-29 23:33:19 +0200

    store number func

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 'use strict';
 
-
-
 /**
  * # is used to select the id of an html element
  * Selecting score elements to manipulate player scores
@@ -18,9 +16,6 @@ const current0E2 = document.getElementById('current--1');
 
 const diceEl = document.querySelector('.dice');
 
-const scores = [0, 0]
-let activePlayer = 0
-
 /**
  * Buttons selections
  */
@@ -28,49 +23,107 @@ const refreshButton = document.querySelector('.btn--new');
 const rollButton = document.querySelector('.btn--roll');
 const holdButton = document.querySelector('.btn--hold');
 
-scoreOne.textContent = 0
-scoreTwo.textContent = 0
-diceEl.classList.add('hidden')
+/**
+ * function used to switch between players
+ * 1.) Updates current player using a ternary condition
+ * 2.)//the toggle function is used to dynamically add and remove
+ *      classes from an HTMl element
+ */
+function switchPlayer() {
+    document.getElementById(`current--${activePlayer}`).textContent = 0
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
 
-let currentScore = 0
+    player0EL.classList.toggle(`player--active`)
+    player1EL.classList.toggle(`player--active`)
+}
+
+let  scores, currentScore, activePlayer, isPlaying;
+/**
+ * This is a function that is used on page load up and new game reset
+ * resets all screen variables to begin the game from scratch
+ */
+function resetGame() {
+    scoreOne.textContent = 0
+    scoreTwo.textContent = 0
+
+    current0E1.textContent = 0
+    current0E2.textContent = 0
+
+    diceEl.classList.add('hidden')
+
+    player0EL.classList.remove('player--winner')
+    player1EL.classList.remove('player--winner')
+
+    player0EL.classList.add('player--active')
+    player1EL.classList.remove('player--active')
+
+     scores = [0, 0]
+    currentScore = 0;
+    activePlayer = 0
+    isPlaying = true
+}
+
+ resetGame()
 
 /**
  * Reacting to the button click from a user [roll button]
  */
 rollButton.addEventListener('click', function () {
 
-    //1.) Generating a random number between 1-6
-    let dice = Math.trunc(Math.random() * 6 + 1);
-    console.log(dice); //remove
+    if (isPlaying) {
+        //1.) Generating a random number between 1-6
+        let dice = Math.trunc(Math.random() * 6 + 1);
+        console.log(dice); //remove
 
-    //2.) Display dice
-    diceEl.classList.remove('hidden')
-    diceEl.src = `dice-${dice}.png`
+        //2.) Display dice
+        diceEl.classList.remove('hidden')
+        diceEl.src = `dice-${dice}.png`
 
-    //3.) Check if user roll 1: if true, switch the other player
-    if (dice !== 1) {
+        //3.) Check if user rolls 1: if true, switch the other player
+        if (dice !== 1) {
 
-        currentScore += dice;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-    } else {
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        } else {
 
-        console.log('User rolled a 1'); //remove
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
+            console.log('User rolled a 1'); //remove
+            switchPlayer();
 
-        //Looks for html and switches the sections background color depending on which player is an active player
-        player0EL.classList.toggle(`player--active`)
-        player1EL.classList.toggle(`player--active`)
+        }
     }
 })
 
 holdButton.addEventListener('click', function () {
-    //1. Add current score to active players
-    scores[activePlayer] += currentScore;
-    document.getElementById(`current--${activePlayer}`).textContent = scores[activePlayer];
 
-    //2. Check if players score s >= 100
-    //Finish game 
+    if (isPlaying) {
+        //1. Add current score to active players
+        scores[activePlayer] += currentScore;
+        //console.log(scores[activePlayer])
+
+        document.getElementById(`score--${activePlayer}`).textContent =
+            scores[activePlayer];
+
+        //2. Check if players score s >= 100
+        if (scores[activePlayer] >= 10) {
+            document.querySelector(`.player--${activePlayer}`)
+                .classList.add('player--winner')
+
+            document.querySelector(`.player--${activePlayer}`)
+                .classList.remove('player--active')
+
+            isPlaying = false
+            diceEl.classList.add('hidden')
+
+            document.querySelector(`#name--${activePlayer}`).textContent = `Player Wins!🏆`
+
+        } else {
+            switchPlayer();
+        }
+    }
 })
 
+//handle button click
+//Reset all values of the game
+
+refreshButton.addEventListener('click',   resetGame);
